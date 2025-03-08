b3cb038982d35a22cee58c08a22f8bfb1d2304d4
nandikanti09 <sriyanandikanti@gmail.com>
2025-03-08 20:53:55 +0530

    Update filename.py

--- filename.py
+++ filename.py
@@ -1,2 +1,11 @@
-text=input("Enter a string:")
-print("Reverse of a String:",text[::-1])
+import os
+
+
+text = os.getenv("MY_INPUT_STRING", "default_value")
+
+
+print(f"Entered string: {text}")
+
+
+reversed_text = text[::-1]
+print(f"Reversed string: {reversed_text}")
